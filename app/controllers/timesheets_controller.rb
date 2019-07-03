7a671daaf78891f4895b9adb4c11238e84afbdae
mariaaxime <mariaaximenaam@gmail.com>
2019-07-03 07:47:09 -0500

    adds form for new entry. adds validation in model and partial for error messages

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -4,9 +4,18 @@ class TimesheetsController < ApplicationController
   end
 
   def new
+    @timesheet = Timesheet.new
   end
 
   def create
+    @timesheet = Timesheet.new(timesheet_params)
+
+  end
+
+  private
+
+  def timesheet_params
+    params.require(:timesheet).permit(:date, :start_time, :finish_time)
   end
 
 end
--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,2 +1,3 @@
 class Timesheet < ApplicationRecord
+  validates_presence_of :date, :start_time, :finish_time
 end
