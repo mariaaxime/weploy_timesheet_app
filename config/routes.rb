9ada95f44f480a4e4afa6138c70dc782bd4eb183
mariaaxime <mariaaximenaam@gmail.com>
2019-07-03 20:59:51 -0500

    declares routes for authenticated and unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'timesheets#index'
-  resources :timesheets, only: [:new, :create]
+
+  devise_scope :user do
+    authenticated :user do
+      root 'timesheets#index'
+
+      resources :timesheets, only: [:new, :create]
+    end
+  end
+
+  unauthenticated :user do
+    root 'devise/sessions#new'
+  end
 end
