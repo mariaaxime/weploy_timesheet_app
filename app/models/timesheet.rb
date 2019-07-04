b4636389bd82a7c41d362d4148a8b6dcafa42669
mariaaxime <mariaaximenaam@gmail.com>
2019-07-03 20:25:20 -0500

    adds method to calculate value

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -4,7 +4,7 @@ class Timesheet < ApplicationRecord
   validate :date_can_not_be_in_the_future
   validate :finish_time_can_not_be_before_start_time
 
-  before_create :calculate_value
+  before_create :set_value
 
   scope :user_id_is, -> (id) { where(user_id: id) }
 
@@ -14,10 +14,15 @@ class Timesheet < ApplicationRecord
 
   private
 
-  def calculate_value
+  def set_value
     if [1, 3, 5].include?(date.wday)
+      duration_in_range = [finish_time, finish_time.change(hour: 19, min: 0)].min - [start_time, start_time.change(hour: 7, min: 0)].max
+      self.value = (duration_in_range / 3600.0 * 22) + ((finish_time - start_time - duration_in_range) / 3600.0 * 33)
     elsif [2, 4].include?(date.wday)
+      duration_in_range = [finish_time, finish_time.change(hour: 17, min: 0)].min - [start_time, start_time.change(hour: 5, min: 0)].max
+      self.value = (duration_in_range / 3600.0 * 25) + ((finish_time - start_time - duration_in_range) / 3600.0 * 35)
     elsif [6, 0].include?(date.wday)
+      self.value = (finish_time - start_time) / 3600.0 * 47
     end
   end
 
